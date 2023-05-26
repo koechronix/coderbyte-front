ef0fc600a0d2419e67311694c223c735549be69d
koechronix <koechronix@yahoo.com>
2023-05-26 16:23:47 +0300

    Styled navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import { Link } from "react-router-dom";
 import PageWrapper from "./PageWrapper";
 import { useCart } from "../../context/cart";
@@ -5,27 +6,38 @@ import { ReactComponent as Cart } from "../../assets/svgs/cart.svg";
 
 const Navbar = () => {
   const { cart } = useCart();
-  const numberOfItemsInInCart = cart?.items?.reduce((a, b) => a + b.quantity, 0) || 0;
-  
+  const numberOfItemsInCart = cart?.items?.reduce((a, b) => a + b.quantity, 0) || 0;
+
+  const handleCartClick = () => {
+    if (!cart?.items?.length) {
+      alert("Please add something to cart!");
+    }
+  };
+
   return (
     <nav className="sticky top-0 z-50 bg-white">
       <PageWrapper>
         <div className="flex flex-col items-center justify-center sm:flex-row sm:items-center sm:justify-between">
-          <Link to="/">
-            <h1 className="relative text-primary-100 font-extrabold text-2xl py-4 pr-0 pl-2 before:content-[''] before:absolute before:left-[40%] before:bottom-4 before:-z-10 before:w-25 before:h-2.5 before:bg-indigo-300/50 before:transform before:-skew-x-12 before:-translate-x-1/2">
+          <Link to="/" className="logo-link">
+            <h1 className="relative text-primary-100 font-logo text-2xl">
               Ticket Master
             </h1>
           </Link>
-          <Link to={cart?.items?.length ? "/checkout" : "#"}>
-            <div className="flex gap-x-2" onClick={() => {
-              if(!cart?.items?.length) alert('please add something to cart!')
-            }}>
+          <Link
+            to={cart?.items?.length ? "/checkout" : "#"}
+            className={`cart-link ${cart?.items?.length ? "" : "disabled"}`}
+            onClick={handleCartClick}
+          >
+            <div className="flex gap-x-2 items-center">
               <div className="w-12 h-12 p-2 bg-gray-200 rounded-full">
                 <Cart className="w-full h-full" />
               </div>
               <div>
                 <h3 className="font-bold text-gray-700">My Cart</h3>
-                <p className="text-gray-500">{numberOfItemsInInCart} {numberOfItemsInInCart <= 1 ? 'item' : 'items'}</p>
+                <p className="text-gray-500">
+                  {numberOfItemsInCart}{" "}
+                  {numberOfItemsInCart <= 1 ? "item" : "items"}
+                </p>
               </div>
             </div>
           </Link>
