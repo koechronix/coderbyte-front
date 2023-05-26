b92687a79e9ac2fe0b0cea5cf00343fea5610626
koechronix <koechronix@yahoo.com>
2023-05-26 14:12:50 +0300

    added svg files

--- src/componets/common/Modal.jsx
+++ src/componets/common/Modal.jsx
@@ -6,11 +6,11 @@ const Modal = ({ paymentProcessing }) => {
     <div className="fixed top-0 left-0 w-full h-full bg-black/60">
       <div className="px-6 py-4 bg-primary-100/90  bg-gradient-to-b w-11/12 sm:max-w-[400px] lg:max-w-[600px] mt-[40%] md:mt-[30%] lg:mt-[20%] mx-auto rounded-xl">
         <h1 className="text-lg font-semibold text-center text-white uppercase sm:text-2xl">
-          {paymentProcessing ? "Payment Processing..." : "Paymen Processed!"}
+          { paymentProcessing ? "Payment Processing..." : "Paymen Processed!"}
         </h1>
 
         <p className="flex justify-center">
-          {paymentProcessing ? (
+          { paymentProcessing ? (
             <Oval
               height={72}
               width={72}
