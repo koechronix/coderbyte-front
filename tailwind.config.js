93c7d2e4d30eb6918f29ecb85b70594ba2139b5b
koechronix <koechronix@yahoo.com>
2023-05-26 15:29:23 +0300

    Adde code to main.sx]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        primary: {
+          50: '#F7FAFC',
+          100: '#5F6AF6',
+          150: '#148016',
+          200: '#026CDF',
+          250: '#ffc439',
+          300: '#FFB3C7',
+          350: '#6F7287',
+        }
+      },
+      width: {
+        25: '6.25rem'
+      },
+    },
   },
   plugins: [],
 }
