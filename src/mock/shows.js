dead710212dadf70b924344b9419d4a1c78c31eb
koechronix <koechronix@yahoo.com>
2023-05-26 13:39:23 +0300

    Created shows and added code

---
+++ src/mock/shows.js
@@ -0,0 +1,34 @@
+import london from '../assets/images/london.jpeg';
+import auckland from '../assets/images/auckland.jpeg';
+import vancouver from '../assets/images/vancouver.jpeg';
+
+
+const shows = [
+    {
+        "id": 1,
+        "title": "The way to Power",
+        "image": london,
+        "price": 300,
+        "quantity": 1,
+        "city": "Nyayo, Nairobi",
+    },
+    {
+        "id": 2,
+        "title": "Hyper Focus",
+        "image": auckland,
+        "price": 400,
+        "quantity": 1,
+        "city": "Kasarani, Nairobi",
+    },
+    {
+        "id": 3,
+        "title": "Atomic Habits",
+        "image": vancouver,
+        "price": 500,
+        "quantity": 1,
+        "city": "Jogoo, Nairobi",
+    }
+    
+]
+
+export default shows;
